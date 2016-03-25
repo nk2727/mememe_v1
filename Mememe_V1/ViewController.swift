7bf4fa541d7c651457df30723742b83deb7c0005
Nicole <nicole@njcdtl01lt3981.itservices.sbc.com>
2016-03-25 16:33:58 -0400

    Initial commit

--- Mememe_V1/ViewController.swift
+++ Mememe_V1/ViewController.swift
@@ -8,18 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func pickAnImage(sender: AnyObject) {
+        
+        //
+        // create an UIImagePickerController
+        //
+        let pickerController = UIImagePickerController()
+        
+        //
+        // set the delegate
+        pickerController.delegate = self
+        
+        //
+        // no editing of the photo
+        // select from photolibrary
+        //
+        pickerController.allowsEditing = false
+        pickerController.sourceType = .PhotoLibrary
+        
+        //
+        // show the image when 'Pick' is clicked
+        //
+        presentViewController(pickerController, animated: true, completion: nil)
     }
 
-
+    //
+    // receiving the image picked by the user from library
+    //
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        //
+        // get the picked image
+        // set ".ScaleAspectFill" so the image would be displayed correctly
+        //
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            //
+            // imageView is the IBOulet defined above
+            //
+            imageView.contentMode = .ScaleAspectFill
+            imageView.image = image
+        
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
-
